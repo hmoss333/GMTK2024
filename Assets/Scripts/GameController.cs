ea01668fa6dcae157777352a00bbab44e683cc89
Henry Moss <henry@flip.fitness>
2024-08-17 00:56:20 -0400

    Created a game controller to handle randomly generating celestial objects; updated player controller so that up/down keys will now rotate the ship on the X axis to allow for 3D flight; spawn system creates objects on all 3 planes and checks distance to make sure no 2 objects are too close; updated fuel logic to always cap out at 0; updated ship prefab

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    readonly float G = 1000f;
+    [SerializeField] GameObject celestialPrefab;
+    [SerializeField] int celestialMax;
+    [SerializeField] int xMax;
+    [SerializeField] int yMax;
+    [SerializeField] int zMax;
+    [SerializeField] List<GameObject> celestialObjs;
+
+
+    private void Awake()
+    {
+        GenerateGalaxy();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Gravity();
+    }
+
+    void GenerateGalaxy()
+    {
+        for (int i = 0; i < celestialMax; i++)
+        {
+            Vector3 randPos = new Vector3(Random.Range(-xMax, xMax), Random.Range(-yMax, yMax), Random.Range(-zMax, zMax));
+            foreach (GameObject celestial in celestialObjs)
+            {
+                float checkDist = Vector3.Distance(randPos, celestial.transform.position);
+                if (checkDist <= 5f)
+                {
+                    i--;
+                    break;
+                }
+            }
+            GameObject tempCelestial = Instantiate(celestialPrefab, randPos, Quaternion.identity);
+            float randScale = Random.Range(20, 50);
+            tempCelestial.transform.localScale = Vector3.one * randScale;
+            tempCelestial.GetComponent<Rigidbody>().mass = randScale;
+            celestialObjs.Add(tempCelestial);
+        }
+    }
+
+    void SetInitialVelocity()
+    {
+        foreach (GameObject a in celestialObjs)
+        {
+            foreach (GameObject b in celestialObjs)
+            {
+                if (!a.Equals(b))
+                {
+                    float m2 = b.GetComponent<Rigidbody>().mass;
+                    float r = Vector3.Distance(a.transform.position, b.transform.position);
+
+                    a.transform.LookAt(b.transform);
+
+                    //Circular Orbit = ((G * M) / r)^0.5, where G = gravitational constant, M is the mass of the central object and r is the distance between the two objects
+                    //We ignore the mass of the orbiting object when the orbiting object's mass is negligible, like the mass of the earth vs. mass of the sun
+                    a.GetComponent<Rigidbody>().velocity += a.transform.right * Mathf.Sqrt((G * m2) / r);
+                }
+            }
+        }
+    }
+
+    void Gravity()
+    {
+        foreach (GameObject a in celestialObjs)
+        {
+            foreach (GameObject b in celestialObjs)
+            {
+                if (!a.Equals(b))
+                {
+                    float m1 = a.GetComponent<Rigidbody>().mass;
+                    float m2 = b.GetComponent<Rigidbody>().mass;
+                    float r = Vector3.Distance(a.transform.position, b.transform.position);
+
+                    a.GetComponent<Rigidbody>().AddForce((b.transform.position - a.transform.position).normalized * (G * (m1 * m2) / (r * r)));
+                }
+            }
+        }
+    }
+}
