36d14b8405bf1b9bdc715ad6f853a26b045ad69a
Henry Moss <henry@flip.fitness>
2024-08-18 09:38:00 -0400

    Moved the laser draw logic to the player controller and changed the system to be a raycast + draw line to now work as a laser; added particle system to celestial bodies that will play when hit by the player's laser; created a new material for the player laser that self-emits and is not effected by shadows; adjusted some player flight control variables to have tighter controls; added logic to increase fuel drain while firing the laser

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -7,26 +7,13 @@ public class CelestialBody : MonoBehaviour
 {
     public float resources;
     Vector3 startScale;
-
-    //Test
-    public LineRenderer Line;
+    [SerializeField] ParticleSystem drainEffect;
 
 
     private void Start()
     {
         resources = transform.localScale.x;
         startScale = transform.localScale;
-
-
-        //Test
-        // set the color of the line
-        Line.startColor = Color.red;
-        Line.endColor = Color.red;
-
-        // set width of the renderer
-        Line.startWidth = 0.3f;
-        Line.endWidth = 0.3f;
-        Line.positionCount = 0;
     }
 
     private void FixedUpdate()
@@ -34,30 +21,13 @@ public class CelestialBody : MonoBehaviour
         transform.localScale = new Vector3(resources, resources, resources);
         if (transform.localScale.x <= 0.0f)
             this.gameObject.SetActive(false);
+    }
 
-
-        float shipDist = Vector3.Distance(transform.position, GameController.instance.ship.transform.position);
-        if (shipDist <= 300f)
-        {
-            //Test
-            if (Line.positionCount == 0)
-            {
-                Line.positionCount = 1;
-                Line.SetPosition(0, transform.position);
-            }
-            else
-            {
-                Line.positionCount = 2;
-                Line.SetPosition(1, GameController.instance.ship.transform.position);
-            }
-
-            print($"Draining from {name}");
-            resources -= 35;
-            GameController.instance.UpdateResources();
-        }
-        else
-        {
-            Line.positionCount = 0;
-        }
+    public void DrainResources()
+    {
+        print($"Draining from {name}");
+        drainEffect.Emit(10);
+        resources -= 15f;//35;
+        GameController.instance.UpdateResources();
     }
 }
